5fed31e9ae991747253d952a92ae235cf2e72945
Bryanmendez21 <bryanmendez21@icloud.com>
2023-11-02 22:31:31 -0400

    Update interestindicator.py

--- interestindicator.py
+++ interestindicator.py
@@ -1,7 +1,4 @@
 def main():
-    print("this is a monthly payment loan calculator")
-    print("")
-
     # collect the necessary inputs: principal, interest rate, years
     principal = float(input("input the loan amount: "))
     apr = float(input("the annual interest rate: "))
@@ -10,12 +7,23 @@ def main():
     # calculate the monthly payment
     monthly_interest_rate = apr / 1200
     amount_of_months = years * 12
-    monthly_payment = (principal * monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-amount_of_months)))
+    monthly_payment = (
+        principal
+        * monthly_interest_rate
+        / (1 - (1 + monthly_interest_rate) ** (-amount_of_months))
+    )
 
     # show to the user
     print("the monthly payment for this loan is: $ %.2f" % monthly_payment)
 
 
+print("this is a monthly payment loan calculator")
+print("")
+run = ["c", "C"]
 # runs program
 while True:
-    main()
+    proceed = input("press c to continue or q to quit: ")
+    if proceed in run:
+        main()
+    elif proceed == "q" or "Q":
+        break
